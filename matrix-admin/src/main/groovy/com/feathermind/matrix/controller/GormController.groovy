c17b969e3f9d4d9a223994dcbf0c339497336fa2
Neo <neoscript@gmail.com>
2020-04-19 21:50:53 +0800

    1.上传附件默认最大20M；2.附件下载中文乱码解决；3.EntityList默认用dateCreated排序，更新后刷新本页；

--- matrix-admin/src/main/groovy/com/feathermind/matrix/controller/GormController.groovy
+++ matrix-admin/src/main/groovy/com/feathermind/matrix/controller/GormController.groovy
@@ -4,7 +4,7 @@ import com.feathermind.matrix.domain.sys.AttachmentInfo
 import com.feathermind.matrix.service.AttachmentService
 import com.feathermind.matrix.service.CasClientService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.CacheControl
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.servlet.view.RedirectView
 
-import javax.activation.FileTypeMap
 import javax.servlet.http.HttpSession
-import java.util.concurrent.TimeUnit
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:3000", "null"], allowCredentials = "true")
@@ -35,11 +33,12 @@ class GormController {
         def info = attachmentService.get(id)
         if (info) {
             def file = attachmentService.getFile(info.fileId)
-            return ResponseEntity.ok()
-                    .header("Content-Disposition", "attachment; filename=" + info.getName())
-                    .cacheControl(CacheControl.maxAge(10, TimeUnit.DAYS).cachePublic().noTransform())
-                    .contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(info.name)))
-                    .body(file.data);
+            HttpHeaders headers = new HttpHeaders();
+            //指定文件名
+            headers.setContentDispositionFormData("attachment", new String(info.name.getBytes(), "ISO-8859-1"));
+            //指定以流的形式下载文件
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            return ResponseEntity.ok().headers(headers).body(file.data);
         } else
             return ResponseEntity.notFound().build()
     }
