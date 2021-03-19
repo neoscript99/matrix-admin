89ac413d203a71ef0cf73e8dbcefc49593416bef
Neo <neoscript@gmail.com>
2021-03-19 15:14:14 +0800

    1.微信unionid为空处理

--- matrix-admin/src/main/groovy/com/feathermind/matrix/service/UserBindService.groovy
+++ matrix-admin/src/main/groovy/com/feathermind/matrix/service/UserBindService.groovy
@@ -9,6 +9,9 @@ import org.springframework.beans.BeanUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
+
+import javax.validation.constraints.NotNull
 
 @Service
 class UserBindService extends AbstractService<UserBind> {
@@ -17,23 +20,25 @@ class UserBindService extends AbstractService<UserBind> {
     @Value('${matrix.defaultRoles}')
     String defaultRoles
 
-    User getOrCreateUser(UserBind newBind) {
+    User getOrCreateUser(@NotNull UserBind newBind) {
+        //根据openid查找，如果已经绑定，更新信息
         def bind = UserBind.findByOpenid(newBind.openid);
-        //如果已经绑定，更新信息
         if (bind) {
             newBind.user = bind.user
             BeanUtils.copyProperties(newBind, bind, GormRepository.domainUpdateIgnores)
             saveEntity(bind)
             return bind.user;
         }
-
-        def uBind = UserBind.findByUnionid(newBind.unionid)
-        if (uBind) {
-            newBind.user = uBind.user
-            saveEntity(newBind)
-            return uBind.user
+        //根据union查找
+        if (StringUtils.hasText(newBind.unionid)) {
+            def uBind = UserBind.findByUnionid(newBind.unionid)
+            if (uBind) {
+                newBind.user = uBind.user
+                saveEntity(newBind)
+                return uBind.user
+            }
         }
-
+        //新用户
         newBind.user = new User(account: newBind.openid, name: newBind.nickname, dept: Department.findBySeq(1)).save()
         saveEntity(newBind)
         roleService.findByCodes(defaultRoles.split(',')).each {
